4a5cb0510bfa68d71fa4168b06393dd3ff13919e
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2018-04-15 13:04:48 -0500

    Se agrega los componentes para realizar notificaciones nativas desde la Web - dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewChild , ElementRef } from '@angular/core';
 import * as io from 'socket.io-client';
 declare var toastr ;
 
@@ -13,19 +13,44 @@ export class AppComponent implements OnInit {
   public socket : SocketIOClient.Socket;
   public socketDataTest : any;
 
+  public notication : any = {
+    title : 'Información',
+    icon  : 'https://goo.gl/KqoDpf', 
+    body : ''
+  }
+
+  @ViewChild("my_notification") my_notification : any;
+
+  public isLocal : boolean = false;
+  public wspoint : string = "";
+  
   constructor() {
-    this.socket = io.connect("http://localhost:8002/socket-app");
+    this.wspoint = (this.isLocal) ? 'http://localhost:8002/socket-app' : '/socket-app'
+    this.socket = io.connect(this.wspoint);
   }
 
   ngOnInit() {
     this.socket.on('psalguero', (data: any) => {
-      toastr.success('Servidor: ' + data.message )      
+      toastr.success('Servidor: ' + data.message)      
+      this.notication.title = 'Información'
+      this.notication.body = data.message;
+      this.my_notification.show()
     })
 
     this.socket.on('test-save', (data) => {
       toastr.info( data.message , 'Notificacion')
+      this.notication.title = 'Nuevo Registro'
+      this.notication.body = data.message;
+      this.my_notification.show()
     })
   }
 
+  cargar( event : any ) {
+    console.log(event)
+  }
+
+  mostrado( event : any ) {
+    console.log( event )
+  }
 
 }
